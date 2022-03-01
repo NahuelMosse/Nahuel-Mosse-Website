d1ab624fd7619b88aedc758c1c3d811427ab64f3
Nahuel Mosse <nahuelgamail@gmail.com>
2022-03-01 10:01:53 -0300

    horizontal scroll

---
+++ main.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("big-container");
+const button_1 = document.getElementById('button_1');
+const button_2 = document.getElementById("button_2");
+const button_3 = document.getElementById("button_3");
+
+container.style.transform="translateX(-25%)";
+
+button_1.addEventListener("click", ()=>{
+    container.style.transform="translateX(8%)";
+});
+button_2.addEventListener("click", ()=>{
+    container.style.transform="translateX(-25%)";
+});
+button_3.addEventListener("click", ()=>{
+    container.style.transform="translateX(-58%)";
+});
