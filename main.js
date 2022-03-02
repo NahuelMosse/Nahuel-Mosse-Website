70a0ea27d6c9f10c735f2fde931b9d535eb7bd4b
Nahuel Mosse <nahuelgamail@gmail.com>
2022-03-02 08:39:11 -0300

    movile nav

---
+++ main.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menu");
+const menuCross = document.getElementById("menu-cross");
+const dropdown = document.querySelector("#menu-dropdown");
+
+menu.addEventListener("click", ()=>{
+    dropdown.style.cssText = "display: flex;";
+});
+
+menuCross.addEventListener("click", ()=>{
+    dropdown.style.cssText = "display: none;";
+});
